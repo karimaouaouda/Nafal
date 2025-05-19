6e5fa6d44c1908531c88ece08e832137ccc10b9b
Karim Aouaouda <karimaouaouda.officiel@gmail.com>
2025-05-19 15:07:14 +0100

    fix bug

--- database/migrations/2024_04_03_114108_create_transactions_table.php
+++ database/migrations/2024_04_03_114108_create_transactions_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('transactions');
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('customer_id')->constrained('customers')->cascadeOnDelete();
